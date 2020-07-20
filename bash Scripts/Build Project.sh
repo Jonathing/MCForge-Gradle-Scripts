7b39e7188c978347560c4fa39da0e9fb8e0e4225
Jonathan Colmenares <jonathing.projects@gmail.com>
2020-07-19 22:12:33 -0400

    finished hub for bash

--- bash Scripts/Build Project.sh
+++ bash Scripts/Build Project.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Clear the screen
-clear
+# Get arguments
+MCGradleArgs=$1
 
-MCGradleAuthor="Jonathing"
-MCGradleVersion="0.4.1"
+if [ "$MCGradleArgs" != "FromHub" ]
+then
+    # Clear the screen
+    clear
 
-# Print script information
-echo "MCGradle Scripts (for GNU bash)"
-echo "Version $MCGradleAuthor"
-echo "Written and Maintained by $MCGradleVersion"
-echo ""
+    MCGradleAuthor="Jonathing"
+    MCGradleVersion="0.4.1"
 
-# Check for update
-. ./internal/check_update.sh
+    # Print script information
+    echo "MCGradle Scripts (for GNU bash)"
+    echo "Version $MCGradleVersion"
+    echo "Written and Maintained by $MCGradleAuthor"
+    echo ""
 
-# Go to root project directory
-cd ../..
+    # Check for update
+    . ./internal/check_update.sh
 
-# Get Forge mod name
-MCProjectName=`grep 'displayName=' src/main/resources/META-INF/mods.toml -m 1`
-MCProjectName=${MCProjectName#*'"'}; MCProjectName=${MCProjectName%'"'*}
+    # Go to root project directory
+    cd ../..
+
+    # Get Forge mod name
+    MCProjectName=`grep 'displayName=' src/main/resources/META-INF/mods.toml -m 1`
+    MCProjectName=${MCProjectName#*'"'}; MCProjectName=${MCProjectName%'"'*}
+fi
 
 # Build the project
 echo "Building $MCProjectName..."
@@ -30,10 +36,15 @@ echo ""
 echo "Finished building $MCProjectName."
 echo "If the build was successful, the output should be located under build\libs"
 
-# Return to scripts directory
-cd Scripts/bash\ Scripts/
+if [ "$MCGradleArgs" != "FromHub" ]
+then
+    # Return to scripts directory
+    cd Scripts/bash\ Scripts/
+    read -s -n 1 -p "Press any key to exit MCGradle Scripts..."
+else
+    read -s -n 1 -p "Press any key to return to the MCGradle Scripts Hub..."
+fi
 
 # END OF SCRIPT
-read -s -n 1 -p "Press any key to exit MCGradle Scripts..."
 echo ""
-exit 0
+echo ""
--- bash Scripts/Set up Eclipse.sh
+++ bash Scripts/Set up Eclipse.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Clear the screen
-clear
+# Get arguments
+MCGradleArgs=$1
 
-MCGradleAuthor="Jonathing"
-MCGradleVersion="0.4.1"
+if [ "$MCGradleArgs" != "FromHub" ]
+then
+    # Clear the screen
+    clear
 
-# Print script information
-echo "MCGradle Scripts (for GNU bash)"
-echo "Version $MCGradleVersion"
-echo "Written and Maintained by $MCGradleAuthor"
-echo ""
+    MCGradleAuthor="Jonathing"
+    MCGradleVersion="0.4.1"
 
-# Check for update
-. ./internal/check_update.sh
+    # Print script information
+    echo "MCGradle Scripts (for GNU bash)"
+    echo "Version $MCGradleVersion"
+    echo "Written and Maintained by $MCGradleAuthor"
+    echo ""
 
-# Go to root project directory
-cd ../..
+    # Check for update
+    . ./internal/check_update.sh
 
-# Get Forge mod name
-MCProjectName=`grep 'displayName=' src/main/resources/META-INF/mods.toml -m 1`
-MCProjectName=${MCProjectName#*'"'}; MCProjectName=${MCProjectName%'"'*}
+    # Go to root project directory
+    cd ../..
+
+    # Get Forge mod name
+    MCProjectName=`grep 'displayName=' src/main/resources/META-INF/mods.toml -m 1`
+    MCProjectName=${MCProjectName#*'"'}; MCProjectName=${MCProjectName%'"'*}
+fi
 
 # Set up the initial Eclipse workspace
 echo "Setting up the initial Eclipse workspace for $MCProjectName..."
@@ -36,10 +42,15 @@ echo ""
 echo "Initial set up for Eclipse complete."
 echo "If you need to generate the run configurations again, run the \"Make Eclipse Runs.ps1\" script."
 
-# Return to scripts directory
-cd Scripts/bash\ Scripts/
+if [ "$MCGradleArgs" != "FromHub" ]
+then
+    # Return to scripts directory
+    cd Scripts/bash\ Scripts/
+    read -s -n 1 -p "Press any key to exit MCGradle Scripts..."
+else
+    read -s -n 1 -p "Press any key to return to the MCGradle Scripts Hub..."
+fi
 
 # END OF SCRIPT
-read -s -n 1 -p "Press any key to exit MCGradle Scripts..."
 echo ""
-exit 0
+echo ""
