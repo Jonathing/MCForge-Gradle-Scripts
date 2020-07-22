1af1823795b89bf146a73363bbc4c93f8f4e7d62
Jonathan Colmenares <jonathing.projects@gmail.com>
2020-07-22 17:32:59 -0400

    Changed where check_update is called

--- MCGradle Scripts.ps1
+++ MCGradle Scripts.ps1
@@ -35,7 +35,11 @@ Write-Host $MCGradleGreeting1
 Write-Host $MCGradleGreeting2
 Write-Host ""
 
+# Check for update
+& '.\PowerShell\internal\check_update.ps1' $MCGradleVersion
+
 $MCWantsToUpdate = 0
+$MCHasUpdated = 0
 Write-Host "An update is available for MCGradle Scripts!"
 do
 {
@@ -50,7 +54,7 @@ do
 
     Write-Host ""
 
-    if ($MCHasChosen -eq 0 -And $MCShowOptionsAgain -eq 0)
+    if ($MCHasChosen -eq 0)
     {
         Write-Host "That's not a valid option." -ForegroundColor Yellow
     }
@@ -66,15 +70,16 @@ if ($MCWantsToUpdate -eq 1)
 
     if (Get-Command git)
     {
+        Write-Host "Downloading MCGradle Scripts..."
         git clone https://github.com/Jonathing/MCGradle-Scripts.git update -q
 
+        Write-Host "Installing MCGradle Scripts..."
         Remove-Item -Force -Recurse '.\Windows\'
         Remove-Item -Force -Recurse '.\PowerShell\'
         Remove-Item -Force -Recurse '.\bash\'
         Remove-Item -Force '.\MCGradle Scripts.bat'
         Remove-Item -Force '.\MCGradle Scripts.ps1'
         Remove-Item -Force '.\MCGradle Scripts.sh'
-        Remove-Item -Force '.\LICENSE'
         Remove-Item -Force '.\CHANGELOG.md'
         Remove-Item -Force '.\README.md'
         Remove-Item -Force '.\UPDATE.md'
@@ -86,155 +91,165 @@ if ($MCWantsToUpdate -eq 1)
         Move-Item -Force '.\update\MCGradle Scripts.bat' '.\MCGradle Scripts.bat'
         Move-Item -Force '.\update\MCGradle Scripts.ps1' '.\MCGradle Scripts.ps1'
         Move-Item -Force '.\update\MCGradle Scripts.sh' '.\MCGradle Scripts.sh'
-        Move-Item -Force '.\update\LICNESE' '.\LICENSE'
         Move-Item -Force '.\update\CHANGELOG.md' '.\CHANGELOG.md'
         Move-Item -Force '.\update\README.md' '.\README.md'
         Move-Item -Force '.\update\UPDATE.md' '.\UPDATE.md'
         Move-Item -Force '.\update\.gitignore' '.\.gitignore'
 
+        Write-Host "Cleaning up..."
         Remove-Item -Force -Recurse .\update\
+
+        Write-Host ""
+        Write-Host "MCGradle Scripts has been successfully updated!"
+        Write-Host "Restarting MCGradle Scripts..."
+
+        Start-Sleep -s 3
+
+        $MCHasUpdated = 1
+
+        & '.\MCGradle Scripts.ps1'
     }
     else
     {
         Write-Host "We weren't able to find git on your system!"
         Write-Host "MCGradle Scripts will not be able to update."
+        Write-Host ""
     }
 
     $ErrorActionPreference = $MCOldPreference
 }
 
-Pause
-
-# Go to root project directory
-Set-Location ..
-
-# Check for update
-& '.\Scripts\PowerShell\internal\check_update.ps1' $MCGradleVersion
-
-# Get Forge mod name
-& '.\Scripts\PowerShell\internal\get_mod_name.ps1'
-$MCProjectName = Get-Content '.\Scripts\PowerShell\internal\MODNAME'
-Remove-Item '.\Scripts\PowerShell\internal\MODNAME'
-
-# Set the title of the Windows PowerShell or PowerShell Core console
-$MCGradleTitle = $MCProjectName + ": MCGradle Scripts Hub"
-[System.Console]::Title = $MCGradleTitle
-
-$MCGradleOptionInfo1 = "Gradle Commands"
-$MCGradleOption1 = "1. Build " + $MCProjectName
-$MCGradleOption2 = "2. Set up your Eclipse workspace"
-$MCGradleOption3 = "3. Set up your IntelliJ IDEA workspace"
-$MCGradleOption4 = "4. Generate the Eclipse run configurations"
-$MCGradleOption5 = "5. Generate the IntelliJ IDEA run configurations"
-$MCGradleOption6 = "6. Do a full cleanup of the workspace"
-$MCGradleOptionInfo2 = "MCGradle Scripts Options"
-$MCGradleOptionC = "C. Clear the screen"
-$MCGradleOptionR = "R. Show the options again"
-$MCGradleOptionA = "A. About MCGradle Scripts"
-$MCGradleOptionQ = "Q. Quit MCGradle Scripts"
-Write-Host "What would you like to do today?"
-Write-Host ""
-$MCShowOptionsAgain = 1
-
-do
+if ($MCHasUpdated -eq 0)
 {
+    # Go to root project directory
+    Set-Location ..
+
+    # Get Forge mod name
+    & '.\Scripts\PowerShell\internal\get_mod_name.ps1'
+    $MCProjectName = Get-Content '.\Scripts\PowerShell\internal\MODNAME'
+    Remove-Item '.\Scripts\PowerShell\internal\MODNAME'
+
+    # Set the title of the Windows PowerShell or PowerShell Core console
+    $MCGradleTitle = $MCProjectName + ": MCGradle Scripts Hub"
+    [System.Console]::Title = $MCGradleTitle
+
+    $MCGradleOptionInfo1 = "Gradle Commands"
+    $MCGradleOption1 = "1. Build " + $MCProjectName
+    $MCGradleOption2 = "2. Set up your Eclipse gay"
+    $MCGradleOption3 = "3. Set up your IntelliJ IDEA workspace"
+    $MCGradleOption4 = "4. Generate the Eclipse run configurations"
+    $MCGradleOption5 = "5. Generate the IntelliJ IDEA run configurations"
+    $MCGradleOption6 = "6. Do a full cleanup of the workspace"
+    $MCGradleOptionInfo2 = "MCGradle Scripts Options"
+    $MCGradleOptionC = "C. Clear the screen"
+    $MCGradleOptionR = "R. Show the options again"
+    $MCGradleOptionA = "A. About MCGradle Scripts"
+    $MCGradleOptionQ = "Q. Quit MCGradle Scripts"
+    Write-Host "What would you like to do today?"
+    Write-Host ""
+    $MCShowOptionsAgain = 1
+
     do
     {
-        if ($MCShowOptionsAgain -eq 1)
+        do
         {
-            Write-Host $MCGradleOptionInfo1
-            Write-Host $MCGradleOption1
-            Write-Host $MCGradleOption2
-            Write-Host $MCGradleOption3
-            Write-Host $MCGradleOption4
-            Write-Host $MCGradleOption5
-            Write-Host $MCGradleOption6
-            Write-Host ""
-            Write-Host $MCGradleOptionInfo2
-            Write-Host $MCGradleOptionC
-            Write-Host $MCGradleOptionR
-            Write-Host $MCGradleOptionA
-            Write-Host $MCGradleOptionQ
-            Write-Host ""
-        }
-        else
-        {
-            Write-Host "Press R to see the options again." -ForegroundColor Yellow
-        }
-        $MCShowOptionsAgain = 0
-        Write-Host "Please pick an option [ 1-6, R, Q, ... ] " -ForegroundColor Yellow -NoNewline
-        $Readhost = Read-Host
-        Switch ($ReadHost)
-        { 
-            1 { $MCHasChosen = 1; $MCGradleCommand = 1 }
-            2 { $MCHasChosen = 1; $MCGradleCommand = 2 }
-            3 { $MCHasChosen = 1; $MCGradleCommand = 3 }
-            4 { $MCHasChosen = 1; $MCGradleCommand = 4 }
-            5 { $MCHasChosen = 1; $MCGradleCommand = 5 }
-            6 { $MCHasChosen = 1; $MCGradleCommand = 6 }
-            C { $MCHasChosen = 1; $MCGradleCommand = 97; $MCShowOptionsAgain = 1 }
-            A { $MCHasChosen = 1; $MCGradleCommand = 98 }
-            Q { $MCHasChosen = 1; $MCGradleCommand = 99 }
-            R { $MCHasChosen = 0; $MCShowOptionsAgain = 1 }
-            Default { $MCHasChosen = 0; $MCGradleCommand = 0 }
-        }
+            if ($MCShowOptionsAgain -eq 1)
+            {
+                Write-Host $MCGradleOptionInfo1
+                Write-Host $MCGradleOption1
+                Write-Host $MCGradleOption2
+                Write-Host $MCGradleOption3
+                Write-Host $MCGradleOption4
+                Write-Host $MCGradleOption5
+                Write-Host $MCGradleOption6
+                Write-Host ""
+                Write-Host $MCGradleOptionInfo2
+                Write-Host $MCGradleOptionC
+                Write-Host $MCGradleOptionR
+                Write-Host $MCGradleOptionA
+                Write-Host $MCGradleOptionQ
+                Write-Host ""
+            }
+            else
+            {
+                Write-Host "Press R to see the options again." -ForegroundColor Yellow
+            }
+            $MCShowOptionsAgain = 0
+            Write-Host "Please pick an option [ 1-6, R, Q, ... ] " -ForegroundColor Yellow -NoNewline
+            $Readhost = Read-Host
+            Switch ($ReadHost)
+            { 
+                1 { $MCHasChosen = 1; $MCGradleCommand = 1 }
+                2 { $MCHasChosen = 1; $MCGradleCommand = 2 }
+                3 { $MCHasChosen = 1; $MCGradleCommand = 3 }
+                4 { $MCHasChosen = 1; $MCGradleCommand = 4 }
+                5 { $MCHasChosen = 1; $MCGradleCommand = 5 }
+                6 { $MCHasChosen = 1; $MCGradleCommand = 6 }
+                C { $MCHasChosen = 1; $MCGradleCommand = 97; $MCShowOptionsAgain = 1 }
+                A { $MCHasChosen = 1; $MCGradleCommand = 98 }
+                Q { $MCHasChosen = 1; $MCGradleCommand = 99 }
+                R { $MCHasChosen = 0; $MCShowOptionsAgain = 1 }
+                Default { $MCHasChosen = 0; $MCGradleCommand = 0 }
+            }
 
-        Write-Host ""
+            Write-Host ""
 
-        if ($MCHasChosen -eq 0 -And $MCShowOptionsAgain -eq 0)
+            if ($MCHasChosen -eq 0 -And $MCShowOptionsAgain -eq 0)
+            {
+                Write-Host "That's not a valid option." -ForegroundColor Yellow
+            }
+        }
+        while ($MCHasChosen -eq 0)
+
+        Switch ($MCGradleCommand)
         {
-            Write-Host "That's not a valid option." -ForegroundColor Yellow
+            1 { & '.\Scripts\PowerShell\build.ps1' "FromHub" }
+            2 { & '.\Scripts\PowerShell\setup_eclipse.ps1' "FromHub" }
+            3 { & '.\Scripts\PowerShell\setup_intellij.ps1' "FromHub" }
+            4 { & '.\Scripts\PowerShell\genEclipseRuns.ps1' "FromHub" }
+            5 { & '.\Scripts\PowerShell\genIntellijRuns.ps1' "FromHub" }
+            6 { & '.\Scripts\PowerShell\full_clean.ps1' "FromHub" }
+            97 { Clear-Host }
+            98
+            {
+                $MCGradleAbout1 = "MCGradle Scripts"
+                $MCGradleAbout2 = "Written and Maintained by " + $MCGradleAuthor
+                $MCGradlePlatformMsg = "Running on " + $MCGradlePlatform
+
+                $MCGradleThanks1 = "Original Windows batch scripts written by Bailey (KingPhygieBoo)"
+
+                Write-Host $MCGradleAbout1
+                Write-Host $MCGradleGreeting2
+                Write-Host $MCGradlePlatformMsg
+                Write-Host $MCGradleAbout2
+                Write-Host ""
+                Write-Host $MCGradleThanks1
+                Write-Host ""
+
+                Pause
+
+                Write-Host ""
+            }
+            99 { Write-Host "Quitting MCGradle Scripts..." -ForegroundColor Red }
+            Default { Write-Host "An error has occured." -ForegroundColor Red }
         }
     }
-    while ($MCHasChosen -eq 0)
+    while ($MCGradleCommand -ne 99)
 
-    Switch ($MCGradleCommand)
+    # Return to scripts directory
+    Set-Location .\Scripts\
+
+    # Revert PowerShell title (Windows Only)
+    if ($PSVersionTable.Platform -eq "Win32NT")
     {
-        1 { & '.\Scripts\PowerShell\build.ps1' "FromHub" }
-        2 { & '.\Scripts\PowerShell\setup_eclipse.ps1' "FromHub" }
-        3 { & '.\Scripts\PowerShell\setup_intellij.ps1' "FromHub" }
-        4 { & '.\Scripts\PowerShell\genEclipseRuns.ps1' "FromHub" }
-        5 { & '.\Scripts\PowerShell\genIntellijRuns.ps1' "FromHub" }
-        6 { & '.\Scripts\PowerShell\full_clean.ps1' "FromHub" }
-        97 { Clear-Host }
-        98
-        {
-            $MCGradleAbout1 = "MCGradle Scripts"
-            $MCGradleAbout2 = "Written and Maintained by " + $MCGradleAuthor
-            $MCGradlePlatformMsg = "Running on " + $MCGradlePlatform
-
-            $MCGradleThanks1 = "Original Windows batch scripts written by Bailey (KingPhygieBoo)"
-
-            Write-Host $MCGradleAbout1
-            Write-Host $MCGradleGreeting2
-            Write-Host $MCGradlePlatformMsg
-            Write-Host $MCGradleAbout2
-            Write-Host ""
-            Write-Host $MCGradleThanks1
-            Write-Host ""
-
-            Pause
-
-            Write-Host ""
-        }
-        99 { Write-Host "Quitting MCGradle Scripts..." -ForegroundColor Red }
-        Default { Write-Host "An error has occured." -ForegroundColor Red }
+        [System.Console]::Title = $MCCurrentTitle
     }
-}
-while ($MCGradleCommand -ne 99)
+    else
+    {
+        [System.Console]::Title = ""
+    }
 
-# Return to scripts directory
-Set-Location .\Scripts\
-
-# Revert PowerShell title (Windows Only)
-if ($PSVersionTable.Platform -eq "Win32NT")
-{
-    [System.Console]::Title = $MCCurrentTitle
-}
-else
-{
-    [System.Console]::Title = ""
+    Write-Host ""
 }
 
-Write-Host ""
 exit 0
